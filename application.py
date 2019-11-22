5ac63d5d0f768e37c9acd119b49ff2f22188324a
gohurali <gohurali@live.com>
2019-11-21 16:51:22 -0800

    [WORKING] application is hosting and working. reports latest BTC price

--- application.py
+++ application.py
@@ -2,21 +2,25 @@ from flask import Flask
 from flask import url_for, jsonify, render_template
 import caller
 import numpy as np
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/price',  methods=['GET'])
+@application.route('/price',  methods=['GET'])
 def price():
     config = caller.get_config()
     contents = caller.fetch_latest_BTC_JSON(config_file=config)
     data_df = caller.parse_alphaV_JSON(contents)
     close_prices = np.array(data_df['4a. close (USD)'].tolist())
-    msg =  jsonify({"Current_Price": close_prices[-1]})
+    msg =  jsonify({"Current_Price": close_prices[0]})
     return msg
 
 if __name__ == "__main__":
-    app.run(port=8080, debug=True)
+    debug = False
+    if(debug):
+        application.run(port=8080, debug=True)
+    elif(debug == False):
+        application.run(host="0.0.0.0")
